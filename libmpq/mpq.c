fc2ec5c0a83106f971602dec06dcdbc50a8bd5d4
Gleb Mazovetskiy <glex.spb@gmail.com>
2023-09-17 08:53:47 +0100

    Support Windows versions without wide char APIs

--- libmpq/mpq.c
+++ libmpq/mpq.c
@@ -39,7 +39,15 @@
 /* support for platform specific things */
 #include "platform.h"
 
-#if (defined(_WIN64) || defined(_WIN32)) && !defined(NXDK)
+#if !defined(LIBMPQ_WINDOWS_NO_WCHAR) && defined(_WIN32)
+#  if (defined(WINVER) && WINVER <= 0x0500 && \
+       !(defined(_WIN32_WINNT) && _WIN32_WINNT > 0))
+// A legacy Windows platform without wide char APIs, e.g. Windows 98, original Xbox.
+#    define LIBMPQ_WINDOWS_NO_WCHAR
+#  endif
+#endif
+
+#if defined(_WIN32) && !defined(LIBMPQ_WINDOWS_NO_WCHAR)
 /* Suppress definitions of `min` and `max` macros by <windows.h>: */
 #define NOMINMAX 1
 #define WIN32_LEAN_AND_MEAN
@@ -82,7 +90,7 @@ const char *libmpq__strerror(int32_t return_code) {
 	return __libmpq_error_strings[-return_code];
 }
 
-#if (defined(_WIN64) || defined(_WIN32)) && !defined(NXDK)
+#if defined(_WIN32) && !defined(LIBMPQ_WINDOWS_NO_WCHAR)
 wchar_t *to_wide_char(const char *str) {
 	const size_t size = strlen(str);
 	const uint32_t flags = MB_ERR_INVALID_CHARS;
@@ -103,7 +111,7 @@ wchar_t *to_wide_char(const char *str) {
 #endif
 
 static FILE *fopen_utf8(const char *filename) {
-#if (defined(_WIN64) || defined(_WIN32)) && !defined(NXDK)
+#if defined(_WIN32) && !defined(LIBMPQ_WINDOWS_NO_WCHAR)
 	wchar_t *filename_wide = to_wide_char(filename);
 	if (filename_wide == NULL) return NULL;
 	FILE *result = _wfopen(filename_wide, L"rb");
